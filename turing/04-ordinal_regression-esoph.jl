b26ad3dd13a7f57e2b51a858436a597be5df6ffb
Jose Storopoli <jose@storopoli.io>
2022-06-30 07:43:24 -0300

    04 - ordinal regression corrected model

--- turing/04-ordinal_regression-esoph.jl
+++ turing/04-ordinal_regression-esoph.jl
@@ -3,7 +3,6 @@ using Bijectors
 using CSV
 using DataFrames
 using LinearAlgebra
-using LazyArrays
 using CategoricalArrays
 
 # reproducibility
@@ -41,23 +40,7 @@ y = esoph[:, :tobgp]
     β ~ filldist(TDist(3) * 2.5, predictors)
 
     # likelihood
-    linear_pred = X * β
-    #y ~ arraydist(LazyArray(@~ OrderedLogistic.(X * β, cutpoints)))
-    y ~ arraydist(LazyArray(@~ OrderedLogistic.(linear_pred, cutpoints)))
-    return (; y, cutpoints, β)
-end
-
-# define the model
-@model function ordered_regression(X, y; predictors=size(X, 2), ncateg=maximum(y))
-    # priors
-    cutpoints ~ Bijectors.ordered(filldist(TDist(3) * 5, ncateg-1))
-    β ~ filldist(TDist(3) * 2.5, predictors)
-
-    # likelihood
-    linear_pred = X * β
-    for i in eachindex(y)
-        y[i] ~ OrderedLogistic(linear_pred[i], cutpoints)
-    end
+    y ~ arraydist([OrderedLogistic(X[i, :] ⋅ β, cutpoints) for i in 1:length(y)])
     return (; y, cutpoints, β)
 end
 
@@ -68,11 +51,12 @@ model = ordered_regression(X, y)
 chn = sample(model, NUTS(), MCMCThreads(), 2_000, 4)
 
 # results:
-#  parameters      mean       std   naive_se      mcse          ess      rhat   ess_per_sec
-#      Symbol   Float64   Float64    Float64   Float64      Float64   Float64       Float64
+#Summary Statistics
+#    parameters      mean       std   naive_se      mcse         ess      rhat   ess_per_sec
+#        Symbol   Float64   Float64    Float64   Float64     Float64   Float64       Float64
 #
-#           α    0.3370    0.0386     0.0004    0.0003   14018.6701    0.9999      218.7341
-#        β[1]    0.5176    0.0468     0.0005    0.0004   12269.1734    0.9998      191.4366
-#        β[2]   -0.3453    0.0407     0.0005    0.0003   13928.3031    0.9997      217.3241
-#        β[3]   -0.0615    0.0377     0.0004    0.0003   14014.8302    0.9999      218.6742
-#        β[4]    0.1703    0.0383     0.0004    0.0003   13054.2746    0.9999      203.6866
+#  cutpoints[1]   -1.4140    0.6359     0.0071    0.0092   4165.4455    1.0002      440.5548
+#  cutpoints[2]   -0.2446    0.6182     0.0069    0.0089   4330.7124    1.0001      458.0341
+#  cutpoints[3]    0.8074    0.6286     0.0070    0.0087   4580.5094    1.0002      484.4537
+#          β[1]   -0.0740    0.1184     0.0013    0.0017   5576.9867    1.0000      589.8452
+#          β[2]   -0.0727    0.1704     0.0019    0.0022   5732.0269    1.0005      606.2429
