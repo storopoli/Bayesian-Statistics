3c4a7ad0c415f85eb3caba2baa3fda1adcf457e0
Jose Storopoli <jose@storopoli.io>
2022-07-07 11:16:00 -0300

    06 - student t regression

--- turing/06-robust_linear_regression-duncan.jl
+++ turing/06-robust_linear_regression-duncan.jl
@@ -29,8 +29,8 @@ y = standardize(ZScoreTransform, y; dims=1)
     ν ~ LogNormal(2, 1)
 
     # likelihood
-    y ~ arraydist(TDist.(ν) .+ α .+ (X * β) .* σ)
-    return(; y, α, β, σ)
+    y ~ arraydist(TDist.(ν) .* σ .+ α .+ (X * β))
+    return(; y, α, β, σ, ν)
 end
 
 # instantiate the model
@@ -40,11 +40,11 @@ model = student_t_regression(X, y)
 chn = sample(model, NUTS(), MCMCThreads(), 2_000, 4)
 
 # results:
-#  parameters      mean       std   naive_se      mcse         ess      rhat   ess_per_sec
+#parameters      mean       std   naive_se      mcse         ess      rhat   ess_per_sec
 #      Symbol   Float64   Float64    Float64   Float64     Float64   Float64       Float64
 #
-#           α    0.0032    0.1448     0.0016    0.0037   1307.6503    1.0060       38.2130
-#        β[1]    1.9482    2.0948     0.0234    0.0808    433.8524    1.0140       12.6783
-#        β[2]    2.0210    1.6720     0.0187    0.0600    612.7589    1.0068       17.9065
-#           σ    0.3803    0.2996     0.0033    0.0221     76.0252    1.0542        2.2217
-#           ν   27.5254   26.4839     0.2961    1.1397    185.8207    1.0230        5.4302
+#           α    0.0028    0.0607     0.0007    0.0007   6302.5129    0.9999      106.9764
+#        β[1]    0.5397    0.1033     0.0012    0.0016   4150.2182    1.0008       70.4442
+#        β[2]    0.4606    0.0997     0.0011    0.0015   4274.3720    1.0012       72.5515
+#           σ    0.3554    0.0684     0.0008    0.0011   3640.0110    1.0001       61.7841
+#           ν   10.4426   13.3598     0.1494    0.1887   3917.3390    1.0001       66.4914
