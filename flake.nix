e23bf1bf6498b0c6a46b8161c8026cf3949f491d
Jose Storopoli <jose@storopoli.io>
2023-12-03 08:01:46 -0300

    fix: ParetoSmooth is not compatible with Turing 0.29

--- flake.nix
+++ flake.nix
@@ -39,7 +39,10 @@
             julia
           ];
 
-          inherit (self.checks.${system}.pre-commit-check) shellHook;
+          shellHook = ''
+            export JULIA_PROJECT="turing"
+            ${self.checks.${system}.pre-commit-check.shellHook}
+          '';
         };
         packages.default = pkgs.stdenvNoCC.mkDerivation rec {
           name = "slides";
